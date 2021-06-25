3578ff19213fa007b8902e9122bc4279e0412096
hiromukariya <86354679+hiromukariya@users.noreply.github.com>
2021-06-25 12:51:10 +0900

    課題提出Task1_1

---
+++ javaStudy/1-1/Task1_1.java
@@ -0,0 +1,14 @@
+public class Task1_1 {
+  public static void main(String[] args) {
+    int hogeInt = 50;
+    String hogeString = "テスト";
+    // double型をhogeDubleという名前の変数を宣言して、値「0.2」で初期化
+    double hogeDouble = 0.2;
+    hogeInt = 100;
+    hogeString = "合格";
+    System.out.println(hogeInt);
+    // 変数hogeStringに格納されている値「合格」を出力
+    System.out.println(hogeString);
+    System.out.println(hogeDouble);
+  }
+}
