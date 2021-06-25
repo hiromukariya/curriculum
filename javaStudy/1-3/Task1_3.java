e1d44dfe123d2bf571f09a58dad3867506d2ee12
hiromukariya <86354679+hiromukariya@users.noreply.github.com>
2021-06-25 15:16:25 +0900

    課題提出Task1_3

---
+++ javaStudy/1-3/Task1_3.java
@@ -0,0 +1,18 @@
+public class Task1_3 {
+  public static void main(String[] args) {
+    int plus = 5 + 10;
+    int minus = 20 - 7;
+    // 変数multiplyに10*2を計算した値「２０」を代入している
+    int multiply = 10 * 2;
+    int remi = 20 % 6;
+    // String型の変数townとlineに「有楽町」「線」という値をそれぞれ代入している
+    String town = "有楽町";
+    String line = "線";
+    String train = town + line;
+    System.out.println(plus);
+    System.out.println(minus);
+    System.out.println(multiply);
+    System.out.println(remi);
+    System.out.println(train);
+  }
+}
