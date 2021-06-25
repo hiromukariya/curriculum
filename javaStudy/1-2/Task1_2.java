3244b9ac11bbad36045c08588c1f4a9433d95f56
hiromukariya <86354679+hiromukariya@users.noreply.github.com>
2021-06-25 13:18:18 +0900

    課題提出Task1_2

---
+++ javaStudy/1-2/Task1_2.java
@@ -0,0 +1,12 @@
+public class Task1_2 {
+  public static void main(String[] args) {
+    String[] country = {"JAPAN","AMERICQ","KOREA"};
+    System.out.println(country.length);
+    String[] strArray = {"りんご","もも","ぶどう"};
+    System.out.println(strArray[1]);
+    // 10,20,30,40,50の要素の値位を初期値とするint型の配列intArrayを作成している
+    int[] intArray = {10, 20, 30, 40, 50};
+    // 配列intArrayの二番目の要素と五番目の要素を足した値を出力している
+    System.out.println(intArray[1] + intArray[4]);
+  }
+}
